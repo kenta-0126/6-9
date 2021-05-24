f1b2d87c24fdb8c55e9c91f20ee7ab7e25f719e0
kenta-0126 <garake_yarou@icloud.com>
2021-05-24 15:36:57 +0000

    [ADD]詳細ページに削除機能 を追加[Finish]画像投稿機能を作ろう

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '66386f528b79f5cdf6f39d73d8dc11a6baf9c82cf15e0d16ab043ab5dcaf3569b092af876675a9ce46dd909e041391f7a7f6d3442cfd4df5d8ba958d3bba70ba'
